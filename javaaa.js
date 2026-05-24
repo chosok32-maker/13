68f894648b411ce4ddd389666e6d2aa4dc788755
chosok32-maker <chosok32@gmail.com>
2026-05-24 23:00:17 +0845

    Add files via upload

---
+++ javaaa.js
@@ -0,0 +1,27 @@
+const IMAGE_SRCS = [
+  "https://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/1.png",
+
+  "https://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/2.png",
+
+  "https://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/3.png",
+
+  "https://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/4.png",
+
+  "https://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/5.png",
+
+  "hhttps://raw.githubusercontent.com/chosok32-maker/13/refs/heads/main/6.png",
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+  "Пиксели",
+
+  "Лапка",
+
+  "Спанч боб",
+
+  "Знаки",
+
+  "Кнопочка",
+
+  "Часы",
+];
